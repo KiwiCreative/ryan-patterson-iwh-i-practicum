1b2929b81af038c7a2076863f9282fe544a32fc1
Ryan Patterson <ryan@kiwicreative.net>
2026-08-17 09:46:16 -0500

    Add custom object homepage

--- index.js
+++ index.js
@@ -13,7 +13,34 @@ const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS_TOKEN;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
-// * Code for Route 1 goes here
+// ROUTE 1 - Display all Book records on the homepage.
+app.get('/', async (req, res) => {
+    const booksUrl = 'https://api.hubapi.com/crm/v3/objects/2-67519817';
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try {
+        const response = await axios.get(booksUrl, {
+            headers,
+            params: {
+                properties: 'name,author,genre',
+                limit: 100
+            }
+        });
+
+        const data = response.data.results;
+
+        res.render('homepage', {
+            title: 'Books | Integrating With HubSpot I Practicum',
+            data
+        });
+    } catch (error) {
+        console.error(error.response?.data || error.message);
+        res.status(500).send('Unable to retrieve books from HubSpot.');
+    }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
